62ceafba72e32ba73b9ba57d735e30e46e1fa1f5
Ethan Miles <ethanmilescs@gmail.com>
2022-04-11 13:28:41 -0500

    api now fethces users

--- api/server.js
+++ api/server.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const Users = require('./users/model')
 
 const server = express()
+
+server.use(express.json())
+
+// console.log(Users) vvvv
+// {
+//     find: [Function: find],
+//     findById: [Function: findById],
+//     insert: [Function: insert],
+//     update: [Function: update],
+//     remove: [Function: remove],
+//     resetDB: [Function: resetDB]
+//   }
+
+server.get('/users', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users)
+        })
+})
+
 
 
 
